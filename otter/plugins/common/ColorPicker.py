06374c17b58cc4a3e2aed8295a6c46de07839e5e
David Andrs <andrsd@gmail.com>
2021-10-14 20:44:01 -0600

    Fixing color picker

--- otter/plugins/common/ColorPicker.py
+++ otter/plugins/common/ColorPicker.py
@@ -28,44 +28,45 @@ class ColorPicker(QtWidgets.QWidgetAction):
 
         w = QtWidgets.QWidget(parent)
 
-        layout = QtWidgets.QGridLayout()
-        layout.setSpacing(10)
-        layout.setContentsMargins(8, 2, 4, 2)
+        self._layout = QtWidgets.QGridLayout()
+        self._layout.setVerticalSpacing(6)
+        self._layout.setHorizontalSpacing(3)
+        self._layout.setContentsMargins(8, 2, 4, 2)
 
         self._color_group = QtWidgets.QButtonGroup()
         self._color_group.setExclusive(True)
         self._color_group.buttonClicked.connect(self.onColorPicked)
 
         # do these first so they get IDs starting from 0
-        btn_layout = self._createLayoutForColors(self._color_tr)
-        layout.addItem(btn_layout, 0, 1)
+        self._layout.setColumnMinimumWidth(3, 5)
+        self._layout.setRowMinimumHeight(1, 5)
 
-        btn_layout = self._createLayoutForColors(self._color_tl)
-        layout.addItem(btn_layout, 0, 0)
+        self._fillLayoutWithColors(0, 4, self._color_tr)
+        self._fillLayoutWithColors(0, 0, self._color_tl)
+        self._fillLayoutWithGreys(2, 0)
+        self._fillLayoutWithColors2(2, 4)
 
-        btn_layout = self._createGreysLayout()
-        layout.addItem(btn_layout, 1, 0)
-
-        btn_layout = self._createColorsLayout()
-        layout.addItem(btn_layout, 1, 1)
-
-        w.setLayout(layout)
+        w.setLayout(self._layout)
+        w.setStyleSheet("""
+            background-color: #fefefe
+            """)
         self.setDefaultWidget(w)
 
-    def _createLayoutForColors(self, colors):
-        layout = QtWidgets.QHBoxLayout()
-        layout.setSpacing(4)
-
+    def _fillLayoutWithColors(self, row, col, colors):
         for clr in colors:
             qclr = QtGui.QColor(clr[0], clr[1], clr[2])
             color_str = qclr.name()
 
             button = QtWidgets.QRadioButton("")
+            button.setFixedSize(16, 16)
             button.setStyleSheet("""
                 QRadioButton::indicator {{
-                    width: 16px;
-                    height: 16px;
+                    width: 14px;
+                    height: 14px;
                     background-color: {};
+                }}
+                QRadioButton::indicator::checked {{
+                    border: 1px solid #000;
                 }}
                 """.format(color_str))
 
@@ -73,15 +74,10 @@ class ColorPicker(QtWidgets.QWidgetAction):
             self._color[self._id] = qclr
             self._id = self._id + 1
 
-            layout.addWidget(button)
+            self._layout.addWidget(button, row, col)
+            col = col + 1
 
-        return layout
-
-    def _createGreysLayout(self):
-        layout = QtWidgets.QGridLayout()
-        layout.setHorizontalSpacing(4)
-        layout.setVerticalSpacing(9)
-
+    def _fillLayoutWithGreys(self, st_row, st_col):
         for row, v in enumerate([0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3]):
             qcolors = [
                 QtGui.QColor.fromHsv(0, 0, int(v * 255)),
@@ -93,47 +89,15 @@ class ColorPicker(QtWidgets.QWidgetAction):
                 color_str = qclr.name()
 
                 button = QtWidgets.QRadioButton("")
+                button.setFixedSize(16, 16)
                 button.setStyleSheet("""
                     QRadioButton::indicator {{
-                        width: 16px;
-                        height: 16px;
-                        background-color: {};
-                    }}
-                    """.format(color_str))
-
-                self._color_group.addButton(button, self._id)
-                self._color[self._id] = qclr
-                self._id = self._id + 1
-
-                layout.addWidget(button, row, col)
-
-        return layout
-
-    def _createColorsLayout(self):
-        layout = QtWidgets.QGridLayout()
-        layout.setHorizontalSpacing(0)
-        layout.setVerticalSpacing(9)
-
-        for row, l in enumerate([0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]):
-            qcolors = []
-            hues = [14, 32, 44, 100, 175, 214, 261, 291]
-            sats = [0.5, 0.6, 0.7, 0.73, 0.75, 0.84, 1.0]
-            for h, s in zip(hues, sats):
-                qclr = QtGui.QColor.fromHsl(h, int(s * 255), int(l * 255))
-                qcolors.append(qclr)
-
-            for col, qclr in enumerate(qcolors):
-                color_str = qclr.name()
-
-                button = QtWidgets.QRadioButton("")
-                button.setStyleSheet("""
-                    QRadioButton::indicator {{
-                        width: 16px;
-                        height: 16px;
+                        width: 14px;
+                        height: 14px;
                         background-color: {};
                     }}
                     QRadioButton::indicator::checked {{
-                        border: 2px solid #000;
+                        border: 1px solid #000;
                     }}
                     """.format(color_str))
 
@@ -141,9 +105,39 @@ class ColorPicker(QtWidgets.QWidgetAction):
                 self._color[self._id] = qclr
                 self._id = self._id + 1
 
-                layout.addWidget(button, row, col)
+                self._layout.addWidget(button, st_row + row, st_col + col)
 
-        return layout
+    def _fillLayoutWithColors2(self, st_row, st_col):
+        hues = [14, 32, 44, 100, 175, 214, 261, 291]
+        for col, hu in enumerate(hues):
+            qcolors = []
+            lightnesses = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
+            saturations = [0.5, 0.6, 0.7, 0.73, 0.75, 0.84, 1.0]
+            for li, sa in zip(lightnesses, saturations):
+                qclr = QtGui.QColor.fromHsl(hu, int(sa * 255), int(li * 255))
+                qcolors.append(qclr)
+
+            for row, qclr in enumerate(qcolors):
+                color_str = qclr.name()
+
+                button = QtWidgets.QRadioButton("")
+                button.setFixedSize(17, 16)
+                button.setStyleSheet("""
+                    QRadioButton::indicator {{
+                        width: 14px;
+                        height: 14px;
+                        background-color: {};
+                    }}
+                    QRadioButton::indicator::checked {{
+                        border: 1px solid #000;
+                    }}
+                    """.format(color_str))
+
+                self._color_group.addButton(button, self._id)
+                self._color[self._id] = qclr
+                self._id = self._id + 1
+
+                self._layout.addWidget(button, st_row + row, st_col + col)
 
     def setColorIndex(self, clr_idx):
         button = self._color_group.button(clr_idx)
